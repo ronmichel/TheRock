e2cb5b53f32bb363daed6b0cdfd20a6f5c668731
Vsevolod Golovko <gvsevolo@amd.com>
2025-08-18 09:43:56 +0200

    run only kernelTuningNet

--- build_tools/github_actions/test_executable_scripts/test_miopen.py
+++ build_tools/github_actions/test_executable_scripts/test_miopen.py
@@ -16,40 +16,7 @@ positive_filter = []
 negative_filter = []
 
 # Fusion #
-positive_filter.append("*Fusion*")
-
-# Batch Normalization #
-positive_filter.append("*/GPU_BNBWD*_*")
-positive_filter.append("*/GPU_BNOCLBWD*_*")
-positive_filter.append("*/GPU_BNFWD*_*")
-positive_filter.append("*/GPU_BNOCLFWD*_*")
-positive_filter.append("*/GPU_BNInfer*_*")
-positive_filter.append("*/GPU_BNOCLInfer*_*")
-positive_filter.append("*/GPU_bn_infer*_*")
-
-# CPU tests
-positive_filter.append("CPU_*")  # tests without a suite
-positive_filter.append("*/CPU_*")  # tests with a suite
-
-# Different
-positive_filter.append("*/GPU_Cat_*")
-positive_filter.append("*/GPU_ConvBiasActiv*")
-
-# Convolutions
-positive_filter.append("*/GPU_Conv*")
-positive_filter.append("*/GPU_conv*")
-
-# Solvers
-positive_filter.append("*/GPU_UnitTestConv*")
-
-negative_filter.append("*DBSync*")
-negative_filter.append("*DeepBench*")
-negative_filter.append("*MIOpenTestConv*")
-
-# Temporary fails
-negative_filter.append("*ConvBiasResAddActivation*")
-negative_filter.append("*ConvFwdBiasResAddActiv*")
-negative_filter.append("*GPU_FusionSetArg_FP16*")
+positive_filter.append("*GPU_KernelTuningNetTestConv*")
 
 gtest_final_filter_cmd = (
     "--gtest_filter=" + ":".join(positive_filter) + "-" + ":".join(negative_filter)
@@ -57,7 +24,7 @@ gtest_final_filter_cmd = (
 
 #############################################
 
-cmd = [f"{THEROCK_BIN_DIR}/miopen_gtest", gtest_final_filter_cmd]
+cmd = [f"MIOPEN_LOG_LEVEL=7 {THEROCK_BIN_DIR}/miopen_gtest", gtest_final_filter_cmd]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmd)}")
 subprocess.run(
     cmd,
