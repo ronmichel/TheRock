456a76003f9fbb89f9c98dcae6e2fe2ba25bf6c4
Mika Laitio <mika.laitio@amd.com>
2025-08-19 16:21:15 -0700

    add FindFFTW3.cmake and FindFFTW3f.cmake
    - add cmake find-file wrappers that will
      the FFTW single and double precision packages
      that are build internally by therock.
    - All cmake variables are set by the cmake
      files provided by the upstream fftw3-project.
    
    Signed-off-by: Mika Laitio <mika.laitio@amd.com>

---
+++ cmake/finders/FindFFTW.cmake
@@ -0,0 +1,15 @@
+# This finder resolves the virtual FFTW package for sub-projects.
+# In reality the FFTW library installed in FFTW3 but we set also the
+# FFTW_INCLUDE_DIRS and FFTW_LIBRARIES env variables without "3"
+cmake_policy(PUSH)
+cmake_policy(SET CMP0057 NEW)
+
+message(STATUS "FindFFTW.cmake: Checking is FFTW in THEROCK_PROVIDED_PACKAGES")
+if("FFTW3" IN_LIST THEROCK_PROVIDED_PACKAGES)
+  cmake_policy(POP)
+  message(STATUS "FindFFTW.cmake: Resolving FFTW3 double precision library version from the super-project")
+  find_package(FFTW3 CONFIG REQUIRED)
+else()
+  cmake_policy(POP)
+  set(FFTW3_FOUND FALSE)
+endif()
