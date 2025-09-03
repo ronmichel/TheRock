6ea7d707c0c0c9794632f4161673ae999030cde8
jbonnell-amd <jason.bonnell@amd.com>
2025-09-03 09:43:51 -0400

    Setup packages and environment in test_rocprofiler_sdk.py

--- build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
+++ build_tools/github_actions/test_executable_scripts/test_rocprofiler_sdk.py
@@ -12,6 +12,37 @@ OUTPUT_ARTIFACTS_DIR = os.getenv("OUTPUT_ARTIFACTS_DIR")
 
 logging.basicConfig(level=logging.INFO)
 
+# Env setup
+env_init_cmd = [
+    "export",
+    "HIP_PLATFORM=amd"
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(env_init_cmd)}")
+subprocess.run(
+    env_init_cmd,
+    cwd=THEROCK_DIR,
+    check=True
+)
+
+# Dependencies setup (remove once Dockerfile is updated)
+dep_init_cmd = [
+    "sudo",
+    "apt-get",
+    "install",
+    "-y",
+    "build-essential",
+    "libdw-dev",
+    "pkg-config",
+    "libopenmpi-dev"
+]
+logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(dep_init_cmd)}")
+subprocess.run(
+    dep_init_cmd,
+    cwd=THEROCK_DIR,
+    check=True
+)
+
+# CMake Init
 cmake_init_cmd = [
     "cmake",
     "-B",
@@ -20,13 +51,13 @@ cmake_init_cmd = [
     f"{OUTPUT_ARTIFACTS_DIR}/share/rocprofiler-sdk/tests"
 ]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmake_init_cmd)}")
-
 subprocess.run(
     cmake_init_cmd,
     cwd=THEROCK_DIR,
     check=True
 )
 
+# Cmake Build
 cmake_cmd = [
     "cmake",
     "--build",
@@ -37,13 +68,13 @@ cmake_cmd = [
     "8"
 ]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(cmake_cmd)}")
-
 subprocess.run(
     cmake_cmd,
     cwd=THEROCK_DIR,
     check=True
 )
 
+# CTest Run Tests
 ctest_cmd = [
     "ctest",
     "--test-dir",
@@ -51,7 +82,6 @@ ctest_cmd = [
     "--output-on-failure"
 ]
 logging.info(f"++ Exec [{THEROCK_DIR}]$ {shlex.join(ctest_cmd)}")
-
 subprocess.run(
     ctest_cmd,
     cwd=THEROCK_DIR,
