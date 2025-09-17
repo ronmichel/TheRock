a2b69c28fcff38bdc622207c9ee5e34d9edc2a6d
raramakr <raramakr@amd.com>
2025-09-17 18:18:24 -0500

    Edit source code so that pkgconfig still refers to libdrm and libdrm_amdgpu
    ROCR-Runtime use pkgconfig to get the drm libraries. The pkgconfig files should load the existing libraries -ldrm and -ldrm_amdgpu rather than the updated library names

--- third-party/sysdeps/linux/libdrm/patch_source.sh
+++ third-party/sysdeps/linux/libdrm/patch_source.sh
@@ -5,5 +5,16 @@ SOURCE_DIR="${1:?Source directory must be given}"
 DRM_MESON_BUILD="$SOURCE_DIR/meson.build"
 AMDGPU_MESON_BUILD="$SOURCE_DIR/amdgpu/meson.build"
 echo "Patching sources..."
-sed -i "s/'drm',/'rocm_sysdeps_drm',/g" $DRM_MESON_BUILD
-sed -i "s/'drm_amdgpu',/'rocm_sysdeps_drm_amdgpu',/g" $AMDGPU_MESON_BUILD
+
+# Replace 'drm' in library() block with 'rocm_sysdeps_drm'
+sed -i -E "/libdrm[[:space:]]*=[[:space:]]*library\(/,/\)/ s/^([[:space:]]*)'drm'[[:space:]]*,/\1'rocm_sysdeps_drm',/" "$DRM_MESON_BUILD"
+# Remove libdrm from pkg.generate block, otherwise it will add '-lrocm_sysdeps_drm to the pkgconfig file
+sed -i "/pkg\.generate\s*(/,/)/ s/\blibdrm,\s*//" "$DRM_MESON_BUILD"
+# Add libraries tag to pkg.generate block
+sed -i "/pkg\.generate\s*(/a\  libraries : ['-L\${libdir}', '-ldrm']," $DRM_MESON_BUILD
+# Replace 'drm_amdgpu' in library() block with 'rocm_sysdeps_drm_amdgpu'
+sed -i -E "/libdrm_amdgpu[[:space:]]*=[[:space:]]*library\(/,/\)/ s/^([[:space:]]*)'drm_amdgpu'[[:space:]]*,/\1'rocm_sysdeps_drm_amdgpu',/" "$AMDGPU_MESON_BUILD"
+# Remove libdrm_amdgpu from pkg.generate block, otherwise it will add '-lrocm_sysdeps_drm_amdgpu to the pkgconfig file
+sed -i "/pkg\.generate\s*(/,/)/ s/\blibdrm_amdgpu,\s*//" "$AMDGPU_MESON_BUILD"
+# Add libraries tag to pkg.generate block
+sed -i "/pkg\.generate\s*(/a\  libraries : ['-L\${libdir}', '-ldrm_amdgpu']," $AMDGPU_MESON_BUILD
