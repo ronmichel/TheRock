131715ea662464d16775cfae453d81e3b2323a3f
Laura Promberger <laura.promberger@amd.com>
2025-10-09 12:18:24 +0000

    some more test changes

--- external-builds/pytorch/skip_tests/pytorch_2.7.py
+++ external-builds/pytorch/skip_tests/pytorch_2.7.py
@@ -5,6 +5,11 @@ skip_tests = {
             "test_unused_output_device_cuda",  # this test does not exist in nightly anymore
             "test_pinned_memory_empty_cache",
             "test_float32_matmul_precision_get_set",
+            # AssertionError: Tensor-likes are not close!
+            # Mismatched elements: 1 / 327680 (0.0%)
+            # Greatest absolute difference: 0.03125 at index (3, 114, 184) (up to 0.01 allowed)
+            # Greatest relative difference: 0.01495361328125 at index (3, 114, 184) (up to 0.01 allowed)
+            "test_index_add_correctness",
         ]
     },
 }
