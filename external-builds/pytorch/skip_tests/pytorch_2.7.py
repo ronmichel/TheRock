84a3bc662fb363b4443e4f529482a4abfb53a729
Laura Promberger <laura.promberger@amd.com>
2025-10-09 11:42:29 +0000

    some more test changes

--- external-builds/pytorch/skip_tests/pytorch_2.7.py
+++ external-builds/pytorch/skip_tests/pytorch_2.7.py
@@ -4,6 +4,7 @@ skip_tests = {
             # Explicitly deselected since givind segfault
             "test_unused_output_device_cuda",  # this test does not exist in nightly anymore
             "test_pinned_memory_empty_cache",
+            "test_float32_matmul_precision_get_set",
         ]
     },
 }
