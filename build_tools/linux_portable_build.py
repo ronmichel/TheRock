7ff2e7c34f28c8ac7cb403536b6081baa422010c
erman-gurses <99776114+erman-gurses@users.noreply.github.com>
2025-11-04 15:08:15 -0800

    Temporary Revert: [ci] Adding gfx1103 coverage #1854 (#2012)
    Clean revert: [ci] Adding gfx1103 coverage #1854
    This is a **temporarily** rollback intended to restore disrupted CI
    service. We plan to re-enable `gfx1103` coverage once the underlying
    issues are resolved and validated.
    Closes #2011

--- build_tools/linux_portable_build.py
+++ build_tools/linux_portable_build.py
@@ -5,7 +5,7 @@ Example usage:
 
     # Build for a specific family. Note that all options after the "--" are
     # passed verbatim to CMake.
-    python linux_build_portable.py -- -DTHEROCK_AMDGPU_FAMILIES=gfx110X-all
+    python linux_build_portable.py -- -DTHEROCK_AMDGPU_FAMILIES=gfx110X-dgpu
 
     # Build with podman vs docker.
     python linux_build_portable.py --docker=podman
--- build_tools/third_party/s3_management/update_dependencies.py
+++ build_tools/third_party/s3_management/update_dependencies.py
@@ -187,7 +187,7 @@ def main() -> None:
     SUBFOLDERS =  [
         "gfx101X-dgpu",
         "gfx103X-dgpu",
-        "gfx110X-all",
+        "gfx110X-dgpu",
         "gfx1150",
         "gfx1151",
         "gfx120X-all",
