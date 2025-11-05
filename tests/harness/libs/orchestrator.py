eb46de8564dcb98a0c565d962cc2e748ed5fc83e
madkasul <108070054+madkasul@users.noreply.github.com>
2025-11-05 10:18:05 +0530

    [Test Harness CI] Initial check-in of code for test harness (#1798)
    ## Motivation
    To do an initial code check-in of test harness
    
    ## Technical Details
    Please refer to documents here:
    https://github.com/ROCm/TheRock/blob/c30482bd05644d332b233e0a24cc50341f374311/docs/development/therock_test_harness.md
    
    ## Test Plan
    We tested via `push: branches:`. After this is checked-in, we can use
    workflow_dispatch to trigger
    
    ## Test Result
    Successfully executed pipeline:
    https://github.com/ROCm/TheRock/actions/runs/18534544897/job/52825996129
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Progress on #1919
    
    ---------
    
    Co-authored-by: Kasula, Madhusudhan <madhusudhan.kasula@amd.com>
    Co-authored-by: geomin12 <geomin12@amd.com>

---
+++ tests/harness/libs/orchestrator.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+
+import logging
+from libs import utils
+
+
+log = logging.getLogger(__name__)
+
+
+class Orchestrator(object):
+    """Orchestrator class to run sharded tests as per the GPUs available"""
+
+    def __init__(self, node):
+        self.node = node
+        self.gpus = node.getGpus()
+        log.info(f"Total GPUs: {len(self.gpus)}")
+
+    def runCtest(self, *args, retries=3, **kwargs):
+        """Runs the CTest based tests in sharded parallel threads"""
+
+        def _runCtest(gpu, tests, *args, **kwargs):
+            """Runs an single CTest shard on an assigned GPU with auto retry of failed tests"""
+            cmd = ("ctest",)
+            for i in range(retries):
+                ret, out, _ = gpu.runCmd(*cmd, *tests, *args, **kwargs)
+                if ret == 0:
+                    return ret, out
+                tests = (*tests, "--rerun-failed")
+                log.info(f"[{gpu.node.host}]: Rerunning Failed Tests")
+            return ret, out
+
+        def _runCtestShards(gpu, shards, iShard, *args, **kwargs):
+            """Runs all the tests in default CTest sharding mode"""
+            tests = ("--tests-information", f"{iShard+1},,{shards}")
+            return _runCtest(gpu, tests, *args, **kwargs)
+
+        # shards tests
+        shards = len(self.gpus)
+        rets = utils.runParallel(
+            *[
+                (_runCtestShards, (gpu, shards, iShard, *args), kwargs)
+                for iShard, gpu in enumerate(self.gpus)
+            ]
+        )
+        # reporting
+        result = True
+        for ret, out in rets:
+            result &= bool(ret == 0)
+        assert result
+        return result
