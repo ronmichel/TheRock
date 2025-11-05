111b81f2e697f9ec8b09bdd1d696b2dfb17fa2d0
madkasul <108070054+madkasul@users.noreply.github.com>
2025-11-05 10:18:05 +0530

    [Test Harness CI] Initial check-in of code for test harness (#1798)
    ## Motivation
    To do an initial code check-in of test harness
    
    ## Technical Details
    Please refer to documents here:
    https://github.com/ROCm/TheRock/blob/c30482bd05644d332b233e0a24cc50341f374311/docs/development/therock_test_harness.md
    
    ## Test Plan
    We tested via `push: branches:`. After this is checked-in, we can use
    workflow_dispatch to trigger
    
    ## Test Result
    Successfully executed pipeline:
    https://github.com/ROCm/TheRock/actions/runs/18534544897/job/52825996129
    
    ## Submission Checklist
    
    - [x] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.
    
    Progress on #1919
    
    ---------
    
    Co-authored-by: Kasula, Madhusudhan <madhusudhan.kasula@amd.com>
    Co-authored-by: geomin12 <geomin12@amd.com>

---
+++ tests/harness/tests_hipcub.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+class TestHipCub:
+    """This is an Pytest Test Suite Class to test hipcub component of TheRock"""
+
+    def test_hipcub(self, orch, therock_path, result):
+        """A Test case to verify hipcub"""
+        result.testVerdict = orch.runCtest(cwd=f"{therock_path}/bin/hipcub")
+        assert result.testVerdict
