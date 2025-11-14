aab2013dd18165bb7e1f3d4205e1bb228ac29d8a
Bain Syrowik <bsyrowik@amd.com>
2025-11-14 09:11:38 -0800

    Increase rocWMMA test timeouts (#2142)
    ## Motivation
    
    When running the full rocWMMA test suite as part of CI for
    rocm-libraries, 15 minutes is not enough.
    
    ## Technical Details
    
    <!-- Explain the changes along with any relevant GitHub links. -->
    
    ## Test Plan
    
    <!-- Explain any relevant testing done to verify this PR. -->
    
    ## Test Result
    
    <!-- Briefly summarize test outcomes. -->
    
    ## Submission Checklist
    
    - [ ] Look over the contributing guidelines at
    https://github.com/ROCm/ROCm/blob/develop/CONTRIBUTING.md#pull-requests.

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -208,7 +208,7 @@ test_matrix = {
     "rocwmma": {
         "job_name": "rocwmma",
         "fetch_artifact_args": "--rocwmma --tests --blas",
-        "timeout_minutes": 15,
+        "timeout_minutes": 60,
         "test_script": f"python {_get_script_path('test_rocwmma.py')}",
         "platform": ["linux", "windows"],
         "total_shards": 4,
