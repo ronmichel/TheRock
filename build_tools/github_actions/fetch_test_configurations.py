0dee84d0663de66d4beba2ad40ae7b3b5661cbed
Jessey Harrymanoharan <jharryma@amd.com>
2025-11-19 05:01:32 -0500

    Add aqlprofile tests fetch_test_configurations.py

--- build_tools/github_actions/fetch_test_configurations.py
+++ build_tools/github_actions/fetch_test_configurations.py
@@ -213,6 +213,15 @@ test_matrix = {
         "platform": ["linux", "windows"],
         "total_shards": 1,
     },
+    # aqlprofile tests
+    "aqlprofile": {
+        "job_name": "apqlprofile",
+        "fetch_artifact_args": "--aqlprofile --tests",
+        "timeout_minutes": 15,
+        "test_script": f"python {_get_script_path('test_aqlprofile.py')}",
+        "platform": ["linux"],
+        "total_shards": 1,
+    },
 }
 
 
