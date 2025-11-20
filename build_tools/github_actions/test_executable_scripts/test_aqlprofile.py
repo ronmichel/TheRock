1daee84311cbf7cc61812d0c33e14f2b9a689690
Jessey Harrymanoharan <jharryma@amd.com>
2025-11-20 18:42:43 -0500

    Update test_aqlprofile.py

--- build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
+++ build_tools/github_actions/test_executable_scripts/test_aqlprofile.py
@@ -11,6 +11,7 @@ logging.basicConfig(level=logging.INFO, format="%(message)s")
 SCRIPT_DIR = Path(__file__).resolve().parent
 THEROCK_DIR = SCRIPT_DIR.parent.parent.parent
 THEROCK_BIN_DIR = os.getenv("THEROCK_BIN_DIR", "")
+env = os.environ.copy()
 platform = os.getenv("RUNNER_OS", "linux").lower()
 
 # Sharding
@@ -18,14 +19,16 @@ env = os.environ.copy()
 env["GTEST_SHARD_INDEX"] = str(int(os.getenv("SHARD_INDEX", "1")) - 1)
 env["GTEST_TOTAL_SHARDS"] = str(int(os.getenv("TOTAL_SHARDS", "1")))
 
-cmd = "run_test.sh"
-TEST_DIR = f"{THEROCK_DIR}/../share/share/hsa-amd-aqlprofile"
+env["LD_LIBRARY_PATH"] = f"../../lib/"
+cmd = "run_tests.sh"
+TEST_DIR = f"{THEROCK_BIN_DIR}/../share/hsa-amd-aqlprofile"
+cmd = f"{THEROCK_BIN_DIR}/../share/hsa-amd-aqlprofile/run_tests.sh"
 
 logging.info(f"++ Exec [{TEST_DIR}]$ {cmd}")
 
 subprocess.run(
     cmd,
-    cwd=TEST_DIR,
+    cwd=THEROCK_DIR,
     check=True,
-    env=environ_vars,
+    env=env,
 )
